ebd7f1640d577444738150506c901cd393121eb5
Edgar Chan <edgarandrei1791@outlook.com>
2019-04-17 20:44:07 -0600

    Migrations created for products and reviews tables

--- database/migrations/2019_04_18_010728_create_products_table.php
+++ database/migrations/2019_04_18_010728_create_products_table.php
@@ -15,6 +15,11 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->text('details');
+            $table->double('price');
+            $table->double('stock');
+            $table->double('discount');
             $table->timestamps();
         });
     }
